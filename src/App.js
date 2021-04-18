85d36da0678fff987f92a45e29d14fcc3a677a92
YaroslavKutsmyda <kutsmydadev@gmail.com>
2021-04-18 13:14:26 +0300

    Add React Router Dom Add Route Add Component Auth

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import {Switch, Route} from 'react-router-dom'
 import Layout from "./hoc/Layout/Layout";
 import Quiz from "./Containers/Quiz/Quiz";
+
 function App() {
     return (
         <Layout>
+            <Switch>
+<Route path='/auth' component={Quiz}/>
+<Route path='/quiz-creactor' component={Quiz}/>
+<Route path='/quiz/:id' component={Quiz}/>
+<Route path='/' component={Quiz}/>
+            </Switch>
             <Quiz/>
         </Layout>
     );
