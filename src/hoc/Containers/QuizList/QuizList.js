9a06d804ec59f284f87e2bd0d598b6b9cb3940e2
YaroslavKutsmyda <kutsmydadev@gmail.com>
2021-04-18 13:19:10 +0300

    Create Components QuizList and QuizCreator

--- src/hoc/Containers/QuizList/QuizList.js
+++ src/hoc/Containers/QuizList/QuizList.js
@@ -1,14 +1,13 @@
 import React, {Component} from 'react';
-import styles from './Auth.module.css'
 
-class Auth extends Component {
+class QuizList extends Component {
     render() {
         return (
             <div>
-                <h1>Auth</h1>
+                
             </div>
         );
     }
 }
 
-export default Auth;
+export default QuizList;
